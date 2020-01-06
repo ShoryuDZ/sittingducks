4bba491728d4f903b0f98356e98a331f89d00ba2
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-01-06 23:36:21 +1100

    Initial Commit
    Created Environment for SittingDucks application to be created

---
+++ SittingDucks/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace SittingDucks
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
